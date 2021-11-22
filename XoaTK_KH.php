e21ba93e33b8433072d3c27fe9d07be183927b50
lonqvu <55783937+lonqvu@users.noreply.github.com>
2021-11-22 20:15:03 +0700

    Update XoaTK_KH.php

--- XoaTK_KH.php
+++ XoaTK_KH.php
@@ -3,5 +3,5 @@ include 'config/db.php';
 	$id = $_GET['id'];
 	$sql = "delete from orders_detail where id_tkkh='$id'";
 	$query = mysqli_query($connect,$sql);
-	header('location: SuaTK_KH');
+	header('location: thongke.php');
  ?>
